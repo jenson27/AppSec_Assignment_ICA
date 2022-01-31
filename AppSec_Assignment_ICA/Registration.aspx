929e6d709ba32e711e51294ae6f03e8331fb2a98
Astral <jenson27@Hotmail.com>
2022-01-31 14:20:43 +0800

    Add project files.

---
+++ AppSec_Assignment_ICA/Registration.aspx
@@ -0,0 +1,136 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Registration.aspx.cs" Inherits="AppSec_Assignment_ICA.Registration" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <script src="https://www.google.com/recaptcha/api.js?render=6Le5jNodAAAAAEbfWDvfn8CtebB_fKqj9ysPiXEF"></script>
+    <script type="text/javascript">
+            function validate() {
+                var str = document.getElementById('<%=tb_password.ClientID %>').value;
+
+                if (str.length < 12) {
+                    document.getElementById("lbl_pwdchecker").innerHTML = "Password Length must e at least 12 characters";
+                    document.getElementById("lbl_pwdchecker").style.color = "Red";
+                    return ("too_short");
+                }
+
+                else if (str.search(/[0-9]/) == -1) {
+                    document.getElementById("lbl_pwdchecker").innerHTML = "Password require at least 1 number";
+                    document.getElementById("lbl_pwdchecker").style.color = "Red";
+                    return ("no_number");
+                }
+
+
+                document.getElementById("lbl_pwdchecker").innerHTML = "Excellent!";
+                document.getElementById("lbl_pwdchecker").style.color = "Blue";
+            }
+    </script>
+</head>
+<body>
+    <form id="form1" runat="server" method="post" enctype="multipart/form-data">
+        <div>
+            <h2>
+                <br />
+                    <asp:Label ID="Label1" runat="server" Text="Account Registration"></asp:Label>
+                <br />
+                <br />
+           </h2>
+        <table class="style1">
+            <tr>
+                <td class="style3">
+                    <asp:Label ID="firstName" runat="server" Text="First Name:"></asp:Label>
+                </td>
+                <td class="style2">
+                    <asp:TextBox ID="tb_firstName" runat="server" Height="36px" Width="280px"></asp:TextBox>
+                </td>
+            </tr>
+            <tr>
+                <td class="style3">
+                    <asp:Label ID="lastName" runat="server" Text="Last Name:"></asp:Label>
+                </td>
+                <td class="style2">
+                    <asp:TextBox ID="tb_lastName" runat="server" Height="36px" Width="280px"></asp:TextBox>
+                </td>
+            </tr>
+            <tr>
+                <td class="style3">
+                    <asp:Label ID="Label3" runat="server" Text="Credit Card Number:"></asp:Label>
+                </td>
+                <td class="style2">
+                    <asp:TextBox ID="tb_creditCard" runat="server" Height="32px" Width="281px"></asp:TextBox>
+                </td>
+            </tr>
+            <tr>
+                <td class="style3">
+                    <asp:Label ID="email" runat="server" Text="Email Address:"></asp:Label>
+                </td>
+                <td class="style2">
+                    <asp:TextBox ID="tb_email" runat="server" Height="32px" Width="281px"></asp:TextBox>
+                </td>
+            </tr>
+            <tr>
+                <td class="style6">
+                    <asp:Label ID="password" runat="server" Text="Password: "></asp:Label>
+                </td>
+                <td class="style7">
+                    <asp:TextBox ID="tb_password" runat="server" Height="32px" Width="281px" onkeyup="javascript:validate()"></asp:TextBox>
+                    <asp:Label ID="lbl_pwdchecker" runat="server" Text="pwdchecker"></asp:Label>
+                </td>
+            </tr>
+            <tr>
+                <td class="style3">
+                    <asp:Label ID="Label6" runat="server" Text="Date Of Birth"></asp:Label>
+                </td>
+                <td class="style2">
+                    <EditItemTemplate>  
+                        <asp:TextBox ID="DOB" runat="server" Text='' TextMode="Date" Height="32px" Width="281px"></asp:TextBox>  
+                    </EditItemTemplate>
+                </td>
+            </tr>
+            <tr>
+                <td class="style3">
+                    <asp:Label ID="Label2" runat="server" Text="Image"></asp:Label>
+                </td>
+                <td>
+                    <fieldset>
+                        <legend> Upload Image </legend>
+                        <label for="Image">Image</label>
+                        <input type="file" name="Image" />
+                    </fieldset>
+                </td>
+            </tr>
+            <tr>
+                <td class="style4">
+                </td>
+                <td class="style5">
+                    <asp:Button ID="btn_Submit" runat="server" Height="48px" 
+                        onclick="btn_Submit_Click" Text="Submit" Width="288px" />
+                </td>
+            </tr>
+    </table>
+
+    <input type="hidden" id="g-recaptcha-response" name="g-recaptcha-response"/>
+
+&nbsp;<br />
+        <asp:Label ID="lbl_gScore" runat="server"></asp:Label>
+        <br />
+        <asp:Label ID="lb_error1" runat="server"></asp:Label>
+        <br />
+        <asp:Label ID="lb_error2" runat="server"></asp:Label>
+    <br />
+        <br />
+    
+    </div>
+    </form>
+
+    <script>
+        grecaptcha.ready(function () {
+            grecaptcha.execute('6Le5jNodAAAAAEbfWDvfn8CtebB_fKqj9ysPiXEF', { action: 'Login' }).then(function (token) {
+                document.getElementById("g-recaptcha-response").value = token;
+            });
+        });
+    </script>
+</body>
+</html>
---
+++ AppSec_Assignment_ICA/Registration.aspx.cs
@@ -0,0 +1,311 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Text.RegularExpressions;
+using System.Drawing;
+using System.Net;
+using System.IO;
+using System.Web.Script.Serialization;
+using System.Web.Services;
+using System.Web.Helpers;
+using System.Security.Cryptography;
+using System.Text;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace AppSec_Assignment_ICA
+{
+    public class MyObject
+    {
+        public string success { get; set; }
+        public List<string> ErrorMessages { get; set; }
+    }
+
+    public partial class Registration : System.Web.UI.Page
+    {
+        string MYDBConnectionString = System.Configuration.ConfigurationManager.ConnectionStrings["MYDBConnection"].ConnectionString;
+        static string finalHash;
+        static string salt;
+        byte[] Key;
+        byte[] IV;
+
+        static string line = "\r";
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        public bool ValidateCaptcha()
+        {
+            bool result = true;
+
+            //When user submits the recaptcha form, the user gets a response POST parameter. 
+            //captchaResponse consist of the user click pattern. Behaviour analytics! AI :) 
+            string captchaResponse = Request.Form["g-recaptcha-response"];
+
+            //To send a GET request to Google along with the response and Secret key.
+            HttpWebRequest req = (HttpWebRequest)WebRequest.Create("https://www.google.com/recaptcha/api/siteverify?secret=6Le5jNodAAAAAFH4D-I1oKKW6dGHELlQsjv74clv &response=" + captchaResponse);
+
+            try
+            {
+
+                //Codes to receive the Response in JSON format from Google Server
+                using (WebResponse wResponse = req.GetResponse())
+                {
+                    using (StreamReader readStream = new StreamReader(wResponse.GetResponseStream()))
+                    {
+                        //The response in JSON format
+                        string jsonResponse = readStream.ReadToEnd();
+
+                        //To show the JSON response string for learning purpose
+                        lbl_gScore.Text = jsonResponse.ToString();
+
+                        JavaScriptSerializer js = new JavaScriptSerializer();
+
+                        //Create jsonObject to handle the response e.g success or Error
+                        //Deserialize Json
+                        MyObject jsonObject = js.Deserialize<MyObject>(jsonResponse);
+
+                        //Convert the string "False" to bool false or "True" to bool true
+                        result = Convert.ToBoolean(jsonObject.success);//
+
+                    }
+                }
+
+                return result;
+            }
+            catch (WebException ex)
+            {
+                throw ex;
+            }
+        }
+
+        /* Returns a score base on the password provided. */
+        private int checkPassword(string password)
+        {
+            int score = 0;
+
+            if (password.Length < 12)
+            {
+                return 1;
+            } else
+            {
+                score++;
+            }
+
+            if (Regex.IsMatch(password, "[a-z]"))
+            {
+                score++;
+            }
+
+            if (Regex.IsMatch(password, "[A-Z]"))
+            {
+                score++;
+            }
+
+            if (Regex.IsMatch(password, "[0-9]"))
+            {
+                score++;
+            }
+
+            if (Regex.IsMatch(password, "^[a-z0-9\\.@#\\$%&]+$"))
+            {
+                score++;
+            }
+
+
+            return score;
+        }
+
+        protected byte[] encryptData(string data)
+        {
+            byte[] cipherText = null;
+            try
+            {
+                RijndaelManaged cipher = new RijndaelManaged();
+                cipher.IV = IV;
+                cipher.Key = Key;
+                ICryptoTransform encryptTransform = cipher.CreateEncryptor();
+                //ICryptoTransform decryptTransform = cipher.CreateDecryptor();
+                byte[] plainText = Encoding.UTF8.GetBytes(data);
+                cipherText = encryptTransform.TransformFinalBlock(plainText, 0, plainText.Length);
+
+
+                //Encrypt
+                //cipherText = encryptTransform.TransformFinalBlock(plainText, 0, plainText.Length);
+                //cipherString = Convert.ToBase64String(cipherText);
+                //Console.WriteLine("Encrypted Text: " + cipherString);
+
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.ToString());
+            }
+
+            finally { }
+            return cipherText;
+        }
+
+        /* Creates an entry in the db. */
+        protected void createAccount(string imageName)
+        {
+
+            try
+            {
+                using (SqlConnection con = new SqlConnection(MYDBConnectionString))
+                {
+                    using (SqlCommand cmd = new SqlCommand("INSERT INTO Account VALUES(@email, @firstName ,@lastName, @creditCard, @passwordHash,@passwordSalt,@dob, @imageName,@iv,@key)"))
+                    //using (SqlCommand cmd = new SqlCommand("INSERT INTO Account VALUES(@Email, @Mobile,@Nric,@PasswordHash,@PasswordSalt,@DateTimeRegistered,@MobileVerified,@EmailVerified)"))
+                    {
+                        using (SqlDataAdapter sda = new SqlDataAdapter())
+                        {
+                            cmd.CommandType = CommandType.Text;
+                            cmd.Parameters.AddWithValue("@email", tb_email.Text.Trim());
+                            cmd.Parameters.AddWithValue("@firstName", tb_firstName.Text.Trim());
+                            cmd.Parameters.AddWithValue("@lastName", tb_lastName.Text.Trim());
+                            cmd.Parameters.AddWithValue("@creditCard", Convert.ToBase64String(encryptData(tb_creditCard.Text.Trim())));
+                            //cmd.Parameters.AddWithValue("@creditCard", tb_creditCard.Text.Trim());
+                            cmd.Parameters.AddWithValue("@passwordHash", finalHash);
+                            cmd.Parameters.AddWithValue("@passwordSalt", salt);
+                            cmd.Parameters.AddWithValue("@dob", DateTime.Now);
+                            cmd.Parameters.AddWithValue("@imageName", imageName.ToString());
+                            cmd.Parameters.AddWithValue("@iv", Convert.ToBase64String(IV));
+                            cmd.Parameters.AddWithValue("@key", Convert.ToBase64String(Key));
+                            cmd.Connection = con;
+                            //con.Open();
+                            //cmd.ExecuteNonQuery();
+                            //con.Close();
+
+                            try
+                            {
+                                con.Open();
+                                cmd.ExecuteNonQuery();
+                                con.Close();
+                            }
+                            catch (Exception ex)
+                            {
+                                //throw new Exception(ex.ToString());
+                                lb_error1.Text = ex.ToString();
+                            }
+                            finally
+                            {
+                                con.Close();
+                            }
+                        }
+                    }
+                }
+
+
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.ToString());
+                //lb_error1.Text = "Invalid input";
+            }
+        }
+
+
+        /* Register Button. */
+        protected void btn_Submit_Click(object sender, EventArgs e)
+        {
+            WebImage photo = null;
+            var newFileName = "";
+            var imagePath = "";
+
+            if (ValidateCaptcha())
+            {
+                /* Password handler. */
+                int scores = checkPassword(tb_password.Text);
+                string status = "";
+                switch (scores)
+                {
+                    case 1:
+                        status = "Very Weak";
+                        break;
+                    case 2:
+                        status = "Weak";
+                        break;
+                    case 3:
+                        status = "Medium";
+                        break;
+                    case 4:
+                        status = "Strong";
+                        break;
+                    case 5:
+                        status = "Very Strong";
+                        break;
+                    default:
+                        break;
+                }
+                lbl_pwdchecker.Text = "Status : " + status;
+                if (scores < 4)
+                {
+                    lbl_pwdchecker.ForeColor = Color.Red;
+                    return;
+                }
+                lbl_pwdchecker.ForeColor = Color.Green;
+
+                /* Session. */
+                if (tb_email.Text.Trim().Equals("email") && tb_password.Text.Trim().Equals("Password12345@"))
+                {
+                    Session["LoggedIn"] = tb_email.Text.Trim();
+
+                    /* Creates a new GUID and save into the session. */
+                    string guid = Guid.NewGuid().ToString();
+                    Session["AuthToken"] = guid;
+
+                    /* Creates new cookie with guid. */
+                    Response.Cookies.Add(new HttpCookie("AuthToken", guid));
+
+                    /* Photo handler. */
+                    photo = WebImage.GetImageFromRequest();
+
+                    if (photo != null)
+                    {
+                        newFileName = guid + "_" + Path.GetFileName(photo.FileName);
+                        imagePath = @"image\" + newFileName;
+                        photo.Save(@"~\" + imagePath);
+                    }
+
+                    /* Password Hashing. */
+                    string pwd = tb_password.Text.ToString().Trim(); ;
+
+                    //Generate random "salt" 
+                    RNGCryptoServiceProvider rng = new RNGCryptoServiceProvider();
+                    byte[] saltByte = new byte[8];
+
+                    //Fills array of bytes with a cryptographically strong sequence of random values.
+                    rng.GetBytes(saltByte);
+                    salt = Convert.ToBase64String(saltByte);
+
+                    SHA512Managed hashing = new SHA512Managed();
+
+                    string pwdWithSalt = pwd + salt;
+                    byte[] plainHash = hashing.ComputeHash(Encoding.UTF8.GetBytes(pwd));
+                    byte[] hashWithSalt = hashing.ComputeHash(Encoding.UTF8.GetBytes(pwdWithSalt));
+
+                    finalHash = Convert.ToBase64String(hashWithSalt);
+
+                    lb_error1.Text = "Salt:" + salt;
+                    lb_error2.Text = "Hash with salt:" + finalHash;
+
+                    RijndaelManaged cipher = new RijndaelManaged();
+                    cipher.GenerateKey();
+                    Key = cipher.Key;
+                    IV = cipher.IV;
+
+
+                    createAccount(newFileName);
+
+                    Response.Redirect("HomePage.aspx", false);
+                } else
+                {
+                    lb_error1.Text = "Wrong email or password";
+                }
+            }
+        }
+    }
+}
