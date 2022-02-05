25424774ac00d75ae72cf052d0fd5aeb2080d5ae
Astral <jenson27@Hotmail.com>
2022-02-05 18:47:26 +0800

    email done

--- AppSec_Assignment_ICA/ForgotPassword.aspx.cs
+++ AppSec_Assignment_ICA/ForgotPassword.aspx.cs
@@ -9,6 +9,11 @@ using System.Net;
 using System.Net.Mail;
 using SendGrid.Helpers.Mail;
 using System.Data.SqlClient;
+using System.Threading.Tasks;
+using System.Text;
+using System.Net.Mime;
+using System.Net.Http;
+using System.Diagnostics;
 
 namespace AppSec_Assignment_ICA
 {
@@ -61,30 +66,57 @@ namespace AppSec_Assignment_ICA
             /* If email exist send a send grid with the random generated number. */
             if (getDBEmail(tb_email.Text.Trim()))
             {
-
-                var apiKey = "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";
-                var client = new SendGridClient(apiKey);
-
-                Random rnd = new Random();
-                int token = rnd.Next(1000, 9999);
-
-                /* Initiate email contents. */
-                var senders = new EmailAddress("testing@AppSec.com", "AppSec");
-                var subject = "Social Lyfe Payment";
-                var recipient = new EmailAddress(tb_email.Text.Trim(), "Jen");
-                var plainTextContent = "Test content";
-                var htmlContent = string.Format("Your verification Code is {0}", token);
-
-                /* Sends email through sendgrid API. */
-                var msg = MailHelper.CreateSingleEmail(senders, recipient, subject, plainTextContent, htmlContent);
-                var response = client.SendEmailAsync(msg).Result;
-
+                SendEmail(tb_email.Text.Trim());
 
             }
             else
             {
                 lb_error.Text = "Email does not exist";
             }
+        }
+
+        static void SendEmail(string email)
+        {
+            Random rnd = new Random();
+            int token = rnd.Next(1000, 9999);
+            /*
+            var apiKey = "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";
+            var client = new SendGridClient(apiKey);
+
+            Random rnd = new Random();
+            int token = rnd.Next(1000, 9999);
+
+            //Initiate email contents.
+            var senders = new EmailAddress("jenson27@hotmail.com", "AppSec");
+            var subject = "Social Lyfe Payment";
+            var recipient = new EmailAddress(email, "Jen");
+            var plainTextContent = "Test content";
+            var htmlContent = string.Format("Your verification Code is {0}", token);
+
+            //Sends email through sendgrid API.
+            var msg = MailHelper.CreateSingleEmail(senders, recipient, subject, plainTextContent, htmlContent);
+            client.SendEmailAsync(msg);
+            */
+
+            // Specify the from and to email address
+            MailMessage mailMessage = new MailMessage("from_email@gmail.com", email);
+            // Specify the email body
+            mailMessage.Body = string.Format("Your verification Code is {0}", token);
+            // Specify the email Subject
+            mailMessage.Subject = "Forgot Password APPSEC";
+
+
+            // No need to specify the SMTP settings as these 
+            // are already specified in web.config
+            SmtpClient smtpClient = new SmtpClient("smtp.gmail.com", 587);
+            //SmtpClient.EnableSsl = true;
+            smtpClient.Credentials = new System.Net.NetworkCredential()
+            {
+                UserName = "astraltest2727@gmail.com",
+                Password = "Astral2727@"
+            };
+            // Finall send the email message using Send() method
+            smtpClient.Send(mailMessage);
         }
     }
 }
